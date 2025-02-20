5f0fa4ffa2c926d4affdd025be06962aef6c971e
artucodadorinsano <godofredobatata12@gmail.com>
2025-02-19 22:15:35 -0300

    Aula 1 Exercicio 3

---
+++ teste3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva a temperatura em graus Fahrenheit que deseja converter para graus Celsius");
+            double fahrenheit = double.Parse(Console.ReadLine());
+
+            double celsius = ((5 * (fahrenheit - 32)) / 9);
+
+            Console.WriteLine("A temperatura em graus celsius convertida de fahrenheit é" + celsius);
+
+            Console.ReadLine();
+        }
+    }
+}
