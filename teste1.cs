85151919942628d1a0a5ed4b92edf0670b8938b2
artucodadorinsano <godofredobatata12@gmail.com>
2025-02-19 21:55:28 -0300

    Exercicio 1 Lista 1

---
+++ teste1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira o tamanho do lado do quadrado");
+            double l = double.Parse(Console.ReadLine());
+
+            double r2 = Math.Sqrt(2);
+            double p = 4 * l;
+            double a = Math.Pow(l, 2);
+            double d = l * r2;
+
+            Console.WriteLine("A área do quadrado é " + a + ", o perímetro é " + p + " e a diagonal é " + d);
+            Console.ReadLine();
+
+        }
+    }
+}
